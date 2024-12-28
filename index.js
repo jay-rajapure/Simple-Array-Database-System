8f38d97403a2b7ea75f94e042b6f8c157cc9d72e
jay-rajapure <jayrajapure758@gmail.com>
2024-12-28 19:16:38 +0530

    adding create table feature

--- index.js
+++ index.js
@@ -1,14 +1,7 @@
 const databases = [];
-      function display_btn(str)
-      {
-        
-        switch (str)
-        {
-            case 'create-db-button':
-                document.getElementById("create-db-input").style.display = "inline"
-                break;
-        }
-
+      function display_section(str)
+      {            
+          document.getElementById(str).style.display = "inline"
       }
 
       function createDataBase(){
@@ -21,4 +14,39 @@ const databases = [];
               console.log(currentDb)
          })
          document.getElementById("db-create-output").innerHTML=`${input_value} is created`
+         
+      } 
+      
+      function showAllDbs()
+      {
+        const table_creation_section = document.getElementById("table-creation-section")
+        if(databases.length === 0 )
+        {
+          const node = document.createElement("b")
+          node.innerHTML = " error : please create a database first " 
+          table_creation_section.appendChild(node)
+          return
+        }
+        
+          const select_db = document.getElementById("select-database")
+          
+          
+          databases.forEach(function(database)
+         {
+            const node = document.createElement("option") 
+            node.innerHTML = database
+            select_db.appendChild(node)
+         }
+        )
+
+        
       }
+      function createTable()
+      {
+          
+      }
+      
+
+      
+
+      
